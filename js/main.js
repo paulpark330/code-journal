df7b532e883aa2c49124962e93476885e8348955
Paul Park <paulpark330@hotmail.com>
2021-07-12 17:51:25 -0700

    user is able to preview the image

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+var $photo = document.querySelector('img');
+
+var $photoInput = document.querySelector('#photo-url');
+
+$photoInput.addEventListener('input', function (event) {
+  $photo.setAttribute('src', event.target.value);
+});
